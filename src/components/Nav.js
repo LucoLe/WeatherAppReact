a5e8b334be69062e89a22f9c7013d5b5183ea749
Lyudmil Lesinski <lyudmilles@gmail.com>
2017-01-31 10:51:08 +0200

    Add active to nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
 
 export default class Nav extends React.Component {
   render() {
     return (
       <div>
         <h2>Nav Component</h2>
-        <Link to="/">Get Weather</Link>
-        <Link to="/about">About</Link>
-        <Link to="/examples">Examples</Link>
+        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
       </div>
     );
   }
